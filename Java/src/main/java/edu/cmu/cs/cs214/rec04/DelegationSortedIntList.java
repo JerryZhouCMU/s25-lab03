0db3c4f6fc23bd61f09667462695dbd2022e3265
JerryZhouSirui <jerryzho@andrew.cmu.edu>
2025-01-30 21:14:14 -0500

    Update Java files

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -6,11 +6,107 @@ package edu.cmu.cs.cs214.rec04;
  * with the current size, which goes down when an element is removed)
  * and exports an accessor (totalAdded) for this count.
  *
- * @author Nora Shoemaker
+ * @author Jerry Zhou
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    /** The real SortedIntList that we delegate to. */
+    private final SortedIntList delegate;
 
+    /** Tracks how many total integers have been added, regardless of removal. */
+    private int totalAdded;
+
+    /**
+     * Default constructor.
+     */
+    public DelegationSortedIntList() {
+        this.delegate = new SortedIntList();
+        this.totalAdded = 0;
+    }
+
+    /**
+     * Adds an integer to the internal SortedIntList and increments the totalAdded counter.
+     *
+     * @param num The integer to add.
+     * @return true if the list changes as a result of this call.
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return delegate.add(num);
+    }
+
+    /**
+     * Adds all elements of the given list to the internal SortedIntList,
+     * incrementing the totalAdded counter for each attempted addition.
+     *
+     * @param list The list of integers to be added.
+     * @return true if the list changes as a result of this call.
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        // We cannot simply call delegate.addAll(list),
+        // because delegate itself doesn't increment any "added" counter.
+        // Instead, we count manually:
+        boolean modified = false;
+        for (int i = 0; i < list.size(); i++) {
+            totalAdded++;
+            modified |= delegate.add(list.get(i));
+        }
+        return modified;
+    }
+
+    /**
+     * Returns the integer at the specified position in the internal list.
+     *
+     * @param index index of the element to return.
+     * @return the element at the specified position in this list.
+     */
+    @Override
+    public int get(int index) {
+        return delegate.get(index);
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from the internal list.
+     *
+     * @param num The element to remove.
+     * @return true if an element was removed.
+     */
+    @Override
+    public boolean remove(int num) {
+        return delegate.remove(num);
+    }
+
+    /**
+     * Removes from the list all elements that are contained in the specified IntegerList.
+     *
+     * @param list The list containing elements to remove.
+     * @return true if this list changed as a result.
+     */
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return delegate.removeAll(list);
+    }
+
+    /**
+     * Returns the number of elements in the internal list.
+     *
+     * @return The size of the internal list.
+     */
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    /**
+     * Returns how many times we've called add(...) since creation.
+     *
+     * @return The total number of attempted additions to this delegated list.
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -6,11 +6,56 @@ package edu.cmu.cs.cs214.rec04;
  * with the current size, which goes down when an element is removed)
  * and exports an accessor (totalAdded) for this count.
  *
- * @author Nora Shoemaker
+ * @author Jerry Zhou
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+    /** Tracks how many total integers have been added, regardless of removal. */
+    private int totalAdded;
 
+    /**
+     * Default constructor.
+     */
+    public InheritanceSortedIntList() {
+        super();
+        this.totalAdded = 0;
+    }
+
+    /**
+     * Adds an integer to the list and increments the totalAdded counter.
+     *
+     * @param num The integer to add.
+     * @return true if the list is changed as a result of the call.
+     */
+    @Override
+    public boolean add(int num) {
+        // We increment the totalAdded count *before* calling super.
+        totalAdded++;
+        return super.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to this list,
+     * and increments the totalAdded counter for each attempted addition.
+     *
+     * @param list The IntegerList containing elements to be added.
+     * @return true if this list changed as a result of the call.
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        // Because super.addAll(...) calls our overridden add(int) for each element,
+        // totalAdded will be incremented once per element there as well.
+        return super.addAll(list);
+    }
+
+    /**
+     * Returns how many times we've called add(...) since creation.
+     *
+     * @return The total number of attempted additions to this list.
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
